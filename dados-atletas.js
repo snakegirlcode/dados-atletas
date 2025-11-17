c65bc4b9582558eb341d342e4f0b104a4594cad4
Thais Campos <168150247+snakegirlcode@users.noreply.github.com>
2025-11-17 16:56:17 -0300

    Update dados-atletas.js

--- dados-atletas.js
+++ dados-atletas.js
@@ -64,16 +64,3 @@ class Atleta {
     return `Média válida: ${this.calculaMediaValida()}`;
   }
 }
-
-const atleta = new Atleta("Cesar Abascal",
-    30, 80, 1.70,
-    [10, 9.34, 8.42, 10, 7.88]);
-
-console.log(atleta.obtemNomeAtleta())
-console.log(atleta.obtemIdadeAtleta())
-console.log(atleta.obtemPesoAtleta())
-console.log(atleta.obtemAlturaAtleta())
-console.log(atleta.obtemNotasAtleta())
-console.log(atleta.obtemCategoria())
-console.log(atleta.obtemIMC())
-console.log(atleta.obtemMediaValida())
