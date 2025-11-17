fa2217f99ac0cbab9f4270b487e320b13b2cc0dd
Thais Campos <168150247+snakegirlcode@users.noreply.github.com>
2025-11-17 16:39:56 -0300

    Create dados-atletas.js

---
+++ dados-atletas.js
@@ -0,0 +1,79 @@
+class Atleta {
+  constructor (nome, idade, peso, altura, notas){
+    this.nome = nome;
+    this.idade = idade;
+    this.peso = peso;
+    this.altura = altura;
+    this.notas = notas;
+  }
+
+  calculaCategoria(){
+    if (this.idade >= 9 && this.idade <= 11) {
+      return "Infantil";
+    } else if (this.idade >= 12 && this.idade <= 13) {
+      return "Juvenil";
+    } else if (this.idade >= 14 && this.idade <= 15) {
+      return "Intermediário";
+    } else if (this.idade >= 16 && this.idade <= 30) {
+      return "Adulto";
+    } else {
+      return "Sem Categoria";
+    }
+  }
+
+  calculaIMC(){
+    return this.peso/(this.altura**2);
+  }
+
+  calculaMediaValida(){
+    let notasOrdenadas = this.notas.toSorted((a, b) => a - b);
+    let notasValidas = notasOrdenadas.slice(1, notasOrdenadas.length - 1);
+    let soma = notasValidas.reduce((total, nota) => total + nota, 0);
+    return soma / notasValidas.length;
+  }
+
+  obtemNomeAtleta(){
+    return `Nome: ${this.nome}`;
+  }
+
+  obtemIdadeAtleta(){
+    return `Idade: ${this.idade}`;
+  }
+
+  obtemPesoAtleta(){
+    return `Peso: ${this.peso}`;
+  }
+
+  obtemAlturaAtleta(){
+    return `Altura: ${this.altura}`
+  }
+
+  obtemNotasAtleta(){
+    return `Notas: ${this.notas.join(', ')}`;
+  }
+
+  obtemCategoria(){
+    return `Categoria: ${this.calculaCategoria()}`;
+  }
+
+  obtemIMC(){
+    return `IMC: ${this.calculaIMC()}`;
+  }
+
+  obtemMediaValida(){
+    return `Média válida: ${this.calculaMediaValida()}`;
+  }
+}
+
+const atleta = new Atleta("Cesar Abascal",
+    30, 80, 1.70,
+    [10, 9.34, 8.42, 10, 7.88]);
+
+console.log(atleta.obtemNomeAtleta())
+console.log(atleta.obtemIdadeAtleta())
+console.log(atleta.obtemPesoAtleta())
+console.log(atleta.obtemAlturaAtleta())
+console.log(atleta.obtemNotasAtleta())
+console.log(atleta.obtemCategoria())
+console.log(atleta.obtemIMC())
+console.log(atleta.obtemMediaValida())
